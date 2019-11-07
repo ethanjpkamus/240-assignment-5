c5e0d013c60c094d4aee1e0afd0665024a20ec6c
Ethan Joseph Kamus <ethanjpkamus@csu.fullerton.edu>
2019-11-06 21:09:49 -0800

    kinda done

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ rm *.out
 rm *.lis
 
 echo "Assemble hypotenuse.asm"
-nasm -f elf64 -l assignment5.lis -o assignment5.o assignment5.asm
+nasm -f elf64 -l hypotenuse.lis -o hypotenuse.o hypotenuse.asm
 
 echo "Compile main.cpp"
 g++ -c -Wall -m64 -std=c++14 -o main.o -fno-pie -no-pie main.cpp
 
 echo "Link all object files"
-g++ -m64 -std=c++14 -fno-pie -no-pie main.o assignment5.o -o myprog.out
+g++ -m64 -std=c++14 -fno-pie -no-pie main.o hypotenuse.o -o myprog.out
 
 echo "Now the program will run"
 ./myprog.out
