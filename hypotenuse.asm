450ef2528028c71b3785d746c29b023b90ec01b0
Ethan Joseph Kamus <ethanjpkamus@csu.fullerton.edu>
2019-12-15 15:16:33 -0800

    idk anymore

--- hypotenuse.asm
+++ hypotenuse.asm
@@ -10,6 +10,7 @@ segment .data
 	showarea db "The area of the triangle is %lf",10,0
 	showhypotenuse db "The hypotenuse has a length of %lf",10,0
 	closing db "The module will now return the hypotenuse to the driver",10,0
+	
 	stringformat db "%s",0
 	twofloat db "%lf %lf",0
 
@@ -20,23 +21,8 @@ segment .text
 
 hypotenuse:
 
-
 	push	rbp
 	mov	rbp, rsp
-
-	;push	rbx
-	;push   rcx
-	;push   rdx
-	;push   rsi
-	;push   rdi
-	;push   r8
-	;push   r9
-	;push   r10
-	;push   r11
-	;push   r12
-	;push   r13
-	;push   r14
-	;push   r15
 
 ;===== print welcome statements ================================================
 
@@ -54,7 +40,6 @@ hypotenuse:
 
 ;===== receive user input ======================================================
 
-	;push	qword 99		;places on the boundary
 	push	qword 0
 	push	qword 0
 	mov 	rax, 0
@@ -77,15 +62,6 @@ hypotenuse:
 	movsd	xmm0, xmm12		;do this becuase printf interacts with the "lowest" xmm registers first
 	movsd	xmm1, xmm13
 	call	printf
-
-;===== do math (old) ================================================================
-
-	;mulsd	xmm12, xmm12		;a^2
-	;mulsd	xmm13, xmm13		;b^2
-
-	;addsd	xmm12, xmm13		;a^2 + b^2 / store in xmm12
-
-	;sqrtsd	xmm11, xmm12		;squareroot of a^2+b^2 / store in xmm11
 
 ;===== Area of Triangle ========================================================
 
@@ -127,20 +103,6 @@ hypotenuse:
 ;===== return everything to main.cpp ===========================================
 
 	movsd	xmm0, xmm11
-
-	;pop	r15
-	;pop	r14
-	;pop    r13
-	;pop    r12
-	;pop    r11
-	;pop    r10
-	;pop    r9
-	;pop    r8
-	;pop    rdi
-	;pop    rsi
-	;pop    rdx
-	;pop    rcx
-	;pop    rbx
 	pop    rbp
 
 	ret
